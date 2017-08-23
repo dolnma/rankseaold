4994c308d89bc68b7d1b7aed75fc4b2bc3f66f4f
Dolnma <dolnma9@gmail.com>
2017-08-23 14:55:12 +0200

    fix

---
+++ anchor/config/app.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    'url' => '/',
+    'index' => '',
+    'timezone' => 'Europe/Prague',
+    'key' => 'KJX65lksUC20Z9PM75q22eGYeq5QvT7J',
+    'language' => 'en_GB',
+    'encoding' => 'UTF-8'
+);
